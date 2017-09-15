c4fc9d0d84eb9d40acb136fcc7c3badc0faf4764
Paweł Sierzputowski <sierzput@gmail.com>
2017-09-15 02:10:44 +0200

    Restore library project guid

--- src/Ninject.Extensions.Factory/Properties/AssemblyInfo.cs
+++ src/Ninject.Extensions.Factory/Properties/AssemblyInfo.cs
@@ -21,6 +21,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security;
 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39527a43-4711-438a-b3a8-63dc45fe45ee")]
 
 #if !NO_PARTIAL_TRUST
 [assembly: AllowPartiallyTrustedCallers]
